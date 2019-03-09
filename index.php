1044e7422e68b8dc4ca50a4fc0c575b47906e39f
Luis14718 <43802987+Luis14718@users.noreply.github.com>
2019-03-09 10:10:00 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,64 @@
+<html>
+	<head>
+<title>Web System</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+	<style>
+	body { border-style: dotted; border-width: 2px;}
+div {border-style: solid; border-width:2px; margin:7px; padding:7px; }
+.left{     padding-top: 200;
+    padding-bottom: 200;	text-align:center }
+.right{ width:50%;
+float: right
+;
+height:400;
+}
+img
+		{
+			width:150px;
+			height:auto;
+			float:right;
+		}
+
+
+.hero 
+		{
+			position: relative;
+			width: 1200px;
+			margin-left: 0;
+			margin-top: 0;
+		}
+	</style>
+	</head>
+	<body>
+	<div >
+			<div class="row">
+				<div class="logo">
+				<img src="logo.png">
+				</div>
+			<ul class="main-nav">
+				<li class="active"><a href=""> Home </a> </li>
+				<li><a href="aboutme.html"> About me </a> </li>
+			</ul>
+			</div>
+			
+			<div class="hero">
+			<h1> Work Study Organizer</h1>
+</div>
+		
+
+		
+		
+
+</div>
+<div class= col>
+<div class= right >
+<img  src="main1.png" alt="Main1 Image">
+</div>
+		<div class= left>
+		Easy to connect
+</div>
+
+
+
+	</body>
+</html>
