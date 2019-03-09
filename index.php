9274da1b59a8a6448002215ee53895b56c799c0f
Luis14718 <43802987+Luis14718@users.noreply.github.com>
2019-03-09 14:25:38 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,101 @@
 <html>
 	<head>
 <title>Web System</title>
-<link href="style.css" rel="stylesheet" type="text/css">
+
 	<style>
+		
+		body
+		{  
+			background-image:linear-gradient(rgba(0, 132, 255, 0),rgba(0, 110, 255, 0)),url(image.jpg);
+			height: auto;
+			background-size: cover;
+			color: white;
+			background-position: center;
+		  
+		}
+		.main-nav li 
+		{
+			display: inline-block;
+			
+		}
+		
+		.main-nav li a 
+		{
+			color: white;
+			text-decoration: none;
+			padding: 5px 20px;
+			font-family: "Roboto", sans-serif;
+			font-size: 15px;
+		}
+		
+		.main-nav li.active a 
+		{
+			border: 1px solid white;
+		}
+		.main-nav li a:hover
+		{
+			border: 1px solid white;
+		}
+		.row
+	{
+		max-width: 1200px;
+		margin: auto;
+	}
+	
+	
+	.main-nav 
+	{
+		float: right;
+		list-style: none;
+		margin-top: 30px;
+	}
+	
+	.main-nav li 
+	{
+		display: inline-block;
+		
+	}
+	
+	.main-nav li a 
+	{
+		color: white;
+		text-decoration: none;
+		padding: 5px 20px;
+		font-family: "Roboto", sans-serif;
+		font-size: 15px;
+	}
+	
+	.main-nav li.active a 
+	{
+		border: 1px solid white;
+	}
+	.main-nav li a:hover
+	{
+		border: 1px solid white;
+	}
+	
+	.logo img
+	{
+		width:150px;
+		height:auto;
+		float: left;
+	}
+	
+	body 
+	{
+		font-family: roboto;
+	}
+		
 	body { border-style: dotted; border-width: 2px;}
+	
 div {border-style: solid; border-width:2px; margin:7px; padding:7px; }
+
 .left{     padding-top: 200;
-    padding-bottom: 200;	text-align:center }
+	padding-bottom: 200;
+		text-align:center; }
+	
+
+
 .right{ width:50%;
 float: right
 ;
@@ -19,14 +108,30 @@ img
 			float:right;
 		}
 
+		h1 
+	{
+		color: white;
+			
+		font-size: 40px;
+		text-align: center;
+		margin-top: 275px;
+		
+	}
 
 .hero 
 		{
 			position: relative;
-			width: 1200px;
+			width: fill;
 			margin-left: 0;
 			margin-top: 0;
 		}
+		.logo img
+		{
+			width:150px;
+			height:auto;
+			float: left;
+		}
+		
 	</style>
 	</head>
 	<body>
@@ -50,6 +155,9 @@ img
 		
 
 </div>
+
+
+
 <div class= col>
 <div class= right >
 <img  src="main1.png" alt="Main1 Image">
