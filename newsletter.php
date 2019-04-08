f6f054bd960aca3f7f468cd0ff577747f41a1359
Luis14718 <43802987+Luis14718@users.noreply.github.com>
2019-04-08 11:29:16 -0600

    los

---
+++ newsletter.php
@@ -0,0 +1,166 @@
+	<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Work-Study Counter</title>
+    
+    <meta http-equiv="Expires" content="0">
+  <meta http-equiv="Last-Modified" content="0">
+  <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+  <meta http-equiv="Pragma" content="no-cache">
+
+	<meta charset="UTF-8">
+	<meta name="description" content="Work study counter">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link href="img/favicon.ico" rel="shortcut icon"/>
+
+
+	<link href="https://fonts.googleapis.com/css?family=Josefin+Sans:400,400i,600,600i,700" rel="stylesheet">
+
+	
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/magnific-popup.css"/>
+	<link rel="stylesheet" href="css/style.css"/>
+
+
+
+</head>
+<body>
+	
+	<div id="preloder">
+		<div class="loader"></div>
+	</div>
+
+
+	<header class="header-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4 col-md-3">
+					<div class="logo">
+					<h2 class="site-logo">Keiser University</h2>
+					</div>
+				</div>
+				<div class="col-lg-8 col-md-9">
+					<p><a href="index.php" class="site-btn header-btn">Log Out</a></p>
+					<nav class="main-menu">
+						<ul>
+							<li><a href="studentview.php">Home</a></li>
+							<li><a href="stopwatch.html">Stop-Watch</a></li>
+							<li><a href="contact.html">Contact</a></li>
+					  </ul>
+				  </nav>
+			  </div>
+			</div>
+		</div>
+		<div class="nav-switch">
+			<i class="fa fa-bars"></i>
+		</div>
+	</header>
+
+	<section class="intro-section">
+		<div class="container text-center">
+			<div class="row">
+				<div class="col-xl-10 offset-xl-1">
+					<h2 class="section-title"><span>Seahawks</span></h2>
+					<h2 class="section-title"> Work-Study Hours Counter</h2>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	<section class="portfolio-section">
+		<div class="container">
+			<ul class="portfolio-filter controls">
+				<li class="control" data-filter="all">All</li>
+				<li class="control" data-filter=".web">Marketing office</li>
+				<li class="control" data-filter=".digital">Bussiness office</li>
+				<li class="control" data-filter=".rened">Library</li>
+				<li class="control" data-filter=".brand">It office		</li>
+			</ul>
+		</div>                       
+		<div class="container-fluid p-md-0 ">
+			<div class="row portfolios-area">
+				<div class="mix col-lg-6 col-md-6 web">
+					<a href="img/portfolio/1.jpg" class="portfolio-item set-bg" data-setbg="img/portfolio/1.jpg">
+						<div class="pi-inner">
+							<h2>+ Show</h2>
+						</div>						
+					</a>
+				</div>
+				<div class="mix col-lg-6 col-md-6 digital">
+					<a href="img/portfolio/2.jpg" class="portfolio-item set-bg " data-setbg="img/portfolio/2.jpg">
+						<div class="pi-inner">
+							<h2>+ Show</h2>
+						</div>						
+					</a>
+				</div>
+				<div class="mix col-lg-4 col-md-6 web">
+					<a href="img/portfolio/3.jpg" class="portfolio-item set-bg" data-setbg="img/portfolio/3.jpg">
+						<div class="pi-inner">
+							<h2>+ Show</h2>
+						</div>						
+					</a>
+				</div>
+				<div class="mix col-lg-4 col-md-6 digital">
+					<a href="img/portfolio/4.jpg" class="portfolio-item set-bg" data-setbg="img/portfolio/4.jpg">
+						<div class="pi-inner">
+							<h2>+ Show</h2>
+						</div>						
+					</a>
+				</div>
+				<div class="mix col-lg-4 col-md-6 rened">
+					<a href="img/portfolio/5.jpg" class="portfolio-item set-bg" data-setbg="img/portfolio/5.jpg">
+						<div class="pi-inner">
+							<h2>+ Show</h2>
+						</div>						
+					</a>
+				</div>
+				<div class="mix col-lg-12 col-md-6 brand">
+					<a href="img/portfolio/6.jpg" class="portfolio-item set-bg" data-setbg="img/portfolio/6.jpg">
+						<div class="pi-inner">
+							<h2>+ Show</h2>
+						</div>						
+					</a>
+				</div>
+				<div class="mix col-lg-6 col-md-6 rened">
+					<a href="img/portfolio/7.jpg" class="portfolio-item set-bg" data-setbg="img/portfolio/7.jpg">
+						<div class="pi-inner">
+							<h2>+ Show</h2>
+						</div>						
+					</a>
+				</div>
+				<div class="mix col-lg-6 col-md-6 brand">
+					<a href="img/portfolio/8.jpg" class="portfolio-item set-bg" data-setbg="img/portfolio/8.jpg">
+						<div class="pi-inner">
+							<h2>+ Show</h2>
+						</div>						
+					</a>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	<footer class="footer-section text-center">
+		<div class="container">
+			<h2 class="section-title mb-5">Let's find your ideal Job </h2>
+			<a href="" class="site-btn">Log Out</a>
+			<div class="credits">
+				<h3><span>Keiser University Latin American Campus</span></h3>
+				<p>Daniel Rodriguez, Juan Bosco, Gandy Domínguez</p>
+			</div>
+
+		</div>
+	</footer>
+
+
+
+
+
+	<script src="js/jquery-2.1.4.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/mixitup.min.js"></script>
+	<script src="js/magnific-popup.min.js"></script>
+	<script src="js/main.js"></script>
+</body>
+</html>
